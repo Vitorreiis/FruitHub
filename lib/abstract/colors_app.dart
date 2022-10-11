aa30e821a1fa2c001085b3650d690c9bd445f43d
CalculadoraIMC <vitorreis0303@gmail.com>
2022-10-11 00:33:48 -0300

    Criando um arquivo.dart para as consts que serão usadas no app

---
+++ lib/abstract/colors_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class ColorsApp {
+  final primaryColor = Color(0xFFFFA451);
+  final textParagraph = Color(0xFF5D577E);
+  final formBackgroundColor = Color(0xFFF5F5F5);
+  final colorTextForm = Color(0xFFC2BDBD);
+  final colorThemeAppBar = Color(0xFF27214D);
+  final colorValorProduct = Color(0xFFF08626);
+  final colorBackCurtida = Color(0xFFFFF7F0);
+  final colorBackaAdicionarRemover = Color(0xFFFFF2E7);
+  final colorBackGroundItem = Color(0xFFFFFAEB);
+
+}
+
---
+++ lib/abstract/images_app.dart
@@ -0,0 +1,4 @@
+class imagesApp {
+  final imageCestaFrutas = 'assets/cesta_frutas.png';
+  final imageCestaFrutas2 = 'assets/cesta_frutas-2.png';
+}
